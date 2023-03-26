e4dea671617257c11231bad9a6339ca25b00a571
RafalKostrzynski <kostrzynskirafal@wp.pl>
2023-03-26 16:17:05 +0200

    Initial commit

---
+++ src/main/java/pl/kostrzynski/nonblockinglayershop/NonBlockingLayerShopApplication.java
@@ -0,0 +1,13 @@
+package pl.kostrzynski.nonblockinglayershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonBlockingLayerShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonBlockingLayerShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kostrzynski/nonblockinglayershop/NonBlockingLayerShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kostrzynski.nonblockinglayershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonBlockingLayerShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
